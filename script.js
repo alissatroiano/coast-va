df420b7c038749dc90ad95ea39dad805db13a495
Alissa Troiano <alissatroianonyc@gmail.com>
2024-08-15 01:38:06 -0400

    import svg

--- script.js
+++ script.js
@@ -8,15 +8,15 @@ document.addEventListener("DOMContentLoaded", function () {
             const nameElement = document.createElement("p");
             nameElement.textContent = "Name: " + data.name;
 
-            const ageElement = document.createElement("p");
-            ageElement.textContent = "Age: " + data.age;
+            const bioElement = document.createElement("p.");
+            bioElement.textContent = " " + data.bio;
 
             const cityElement = document.createElement("p");
             cityElement.textContent = "City: " + data.city;
 
             // Append the elements to the "dataDisplay" div
             dataDisplay.appendChild(nameElement);
-            dataDisplay.appendChild(ageElement);
+            dataDisplay.appendChild(bioElement);
             dataDisplay.appendChild(cityElement);
         })
         .catch(error => console.error("Error fetching JSON data:", error));
