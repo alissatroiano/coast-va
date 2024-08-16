579ffc3f645f5156fb79f03ec3db8612d12d9d09
Alissa Troiano <alissatroianonyc@gmail.com>
2024-08-16 03:12:16 -0400

    Clean up directory

--- data.js
+++ data.js
@@ -6,6 +6,11 @@ document.addEventListener("DOMContentLoaded", function () {
             const navItem = document.querySelector("a.nav-link-cta");
             navItem.href = links[0].url;
             navItem.textContent = links[0].label;
+
+            // Update the register button in the carousel
+            const registerCta = document.getElementById("registerCta");
+            registerCta.href = links[0].url;
+            registerCta.textContent = links[0].label;
         })
 
         .catch(error => console.error("Error fetching JSON data:", error));
